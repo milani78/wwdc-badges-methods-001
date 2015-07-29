01e743b8dcb3a9ae5435dac1c8adb00336822d96
milani78 <ingakyt@gmail.com>
2015-07-29 11:41:16 -0400

    Passed tests

--- WWDCBadgesMethods/FISAppDelegate.m
+++ WWDCBadgesMethods/FISAppDelegate.m
@@ -15,10 +15,37 @@
     return YES;
 }
 
-/*
- 
- * Define your methods between application:didFinishLaunchingWithOptions and the @end marker
- 
- */
+
+//PASSED!
+- (NSString *)makeBadgeForSpeaker:(NSString *)speaker {
+    NSString *badge = [NSString stringWithFormat:@"Hello, my name is %@.", speaker];
+    return badge;
+}
+
+
+//PASSED!
+- (NSMutableArray *)makeAllBadgesForSpeakers:(NSArray *)speakers {
+    NSMutableArray *badges = [[NSMutableArray alloc] init];
+    for (NSUInteger i=0; i<8; i++) {
+        NSString *new = speakers[i];
+        NSString *badge = [self makeBadgeForSpeaker:new]; // Will this badge conflict with the  badge string above? No, It's in a different method which means they're in separate scopes.
+        [badges addObject:badge];
+    }
+    return badges;
+}
+
+
+//PASSED!
+- (NSMutableArray *)greetAndAssignRoomsToSpeakers:(NSArray *)speakers {
+    NSMutableArray *greeting = [[NSMutableArray alloc] init];
+    for (NSUInteger i=0; i<8; i++) {
+        NSString *new = speakers[i];
+        NSString *greetingString = [NSString stringWithFormat:@"Welcome, %@! You'll be in dressing room %lu.", new, i+1];
+        [greeting addObject:greetingString];
+    }
+    return greeting;
+}
+
+
 
 @end
